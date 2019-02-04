42dfd0ff6e6863f041501bdebb59e15976d55127
jameskleeh <james.kleeh@gmail.com>
2019-02-04 12:30:44 -0500

    Use the standard messaging acknowledgement. Deprecate the old class

---
+++ kafka/src/main/java/io/micronaut/configuration/kafka/KafkaAcknowledgement.java
@@ -0,0 +1,22 @@
+package io.micronaut.configuration.kafka;
+
+import io.micronaut.messaging.acknowledgement.Acknowledgement;
+
+/**
+ * Defines an interface that can be injected into {@link io.micronaut.configuration.kafka.annotation.KafkaListener} beans so that offsets can be manually committed.
+ *
+ * @author James Kleeh
+ * @since 1.1.0
+ */
+public interface KafkaAcknowledgement extends Acknowledgement, io.micronaut.configuration.kafka.Acknowledgement {
+
+    /**
+     * Kafka does not support rejection of messages explicitly.
+     *
+     * @throws UnsupportedOperationException if called
+     */
+    @Override
+    default void nack() {
+        throw new UnsupportedOperationException("Kafka does not support rejection of messages explicitly");
+    }
+}
