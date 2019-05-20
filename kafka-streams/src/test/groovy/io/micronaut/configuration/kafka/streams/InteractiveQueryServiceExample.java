e658d74f7b3c5154d311a243822d3304c8b452f8
Christian Oestreich <christian.oestreich@optum.com>
2019-05-20 11:25:29 -0500

    Returning Optional from state store method.

--- kafka-streams/src/test/groovy/io/micronaut/configuration/kafka/streams/InteractiveQueryServiceExample.java
+++ kafka-streams/src/test/groovy/io/micronaut/configuration/kafka/streams/InteractiveQueryServiceExample.java
@@ -1,8 +1,10 @@
 package io.micronaut.configuration.kafka.streams;
 
 import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
 
 import javax.inject.Singleton;
+import java.util.Optional;
 
 /**
  * Example service that uses the InteractiveQueryService in a reusable way.  This is only intended as an example.
@@ -20,32 +22,35 @@ public class InteractiveQueryServiceExample {
      * Method to get the word state store and word count from the store using the interactive query service.
      *
      * @param stateStore the name of the state store ie "foo-store"
-     * @param word the key to get, in this case the word as the stream and ktable have been grouped by word
+     * @param word       the key to get, in this case the word as the stream and ktable have been grouped by word
      * @return the Long count of the word in the store
      */
     public Long getWordCount(String stateStore, String word) {
-        return interactiveQueryService.getQueryableStore(stateStore, QueryableStoreTypes.<String, Long>keyValueStore()).get(word);
+        Optional<ReadOnlyKeyValueStore<String, Long>> queryableStore = interactiveQueryService.getQueryableStore(stateStore, QueryableStoreTypes.keyValueStore());
+        return queryableStore.map(kvReadOnlyKeyValueStore -> kvReadOnlyKeyValueStore.get(word)).orElse(0L);
     }
 
     /**
      * Method to get byte array from a state store using the interactive query service.
      *
      * @param stateStore the name of the state store ie "bar-store"
-     * @param blobName the key to get, in this case the name of the blob
+     * @param blobName   the key to get, in this case the name of the blob
      * @return the byte[] stored in the state store
      */
     public byte[] getBytes(String stateStore, String blobName) {
-        return interactiveQueryService.getQueryableStore(stateStore, QueryableStoreTypes.<String, byte[]>keyValueStore()).get(blobName);
+        Optional<ReadOnlyKeyValueStore<String, byte[]>> queryableStore = interactiveQueryService.getQueryableStore(stateStore, QueryableStoreTypes.keyValueStore());
+        return queryableStore.map(stringReadOnlyKeyValueStore -> stringReadOnlyKeyValueStore.get(blobName)).orElse(null);
     }
 
     /**
      * Method to get value V by key K.
      *
      * @param stateStore the name of the state store ie "baz-store"
-     * @param name the key to get
+     * @param name       the key to get
      * @return the value of type V stored in the state store
      */
-    public <K,V> V getGenericKeyValue(String stateStore, K name) {
-        return interactiveQueryService.getQueryableStore(stateStore, QueryableStoreTypes.<K, V>keyValueStore()).get(name);
+    public <K, V> V getGenericKeyValue(String stateStore, K name) {
+        Optional<ReadOnlyKeyValueStore<K, V>> queryableStore = interactiveQueryService.getQueryableStore(stateStore, QueryableStoreTypes.<K, V>keyValueStore());
+        return queryableStore.map(kvReadOnlyKeyValueStore -> kvReadOnlyKeyValueStore.get(name)).orElse(null);
     }
 }
