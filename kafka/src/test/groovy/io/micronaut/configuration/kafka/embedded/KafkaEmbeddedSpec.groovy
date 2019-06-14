79ff5f93e4383a6850fea3309044317edeb47352
Krzysztof Ziomek <kziomek@onet.eu>
2019-06-14 20:51:35 +0100

    Issue #41 use num.partitions property to create multiple partition topic in embedded kafka

--- kafka/src/test/groovy/io/micronaut/configuration/kafka/embedded/KafkaEmbeddedSpec.groovy
+++ kafka/src/test/groovy/io/micronaut/configuration/kafka/embedded/KafkaEmbeddedSpec.groovy
@@ -18,10 +18,12 @@ package io.micronaut.configuration.kafka.embedded
 import io.micronaut.configuration.kafka.config.AbstractKafkaConfiguration
 import io.micronaut.configuration.kafka.config.AbstractKafkaConsumerConfiguration
 import io.micronaut.context.ApplicationContext
+import org.apache.kafka.clients.CommonClientConfigs
+import org.apache.kafka.clients.admin.AdminClient
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import spock.lang.Specification
 
-class KafkaEmbeddedSpec extends Specification{
+class KafkaEmbeddedSpec extends Specification {
 
     void "test run kafka embedded server"() {
         given:
@@ -48,5 +50,47 @@ class KafkaEmbeddedSpec extends Specification{
 
         cleanup:
         applicationContext.close()
+    }
+
+    void "test run kafka embedded server with multiple partitions topic"() {
+        given:
+        int partitionNumber = 10
+        String topicName = "my-topic"
+
+        ApplicationContext applicationContext = ApplicationContext.run(
+                new HashMap() {
+                    {
+                        put(AbstractKafkaConfiguration.EMBEDDED, true)
+                        put(AbstractKafkaConfiguration.EMBEDDED_TOPICS, topicName)
+                        put("kafka.embedded.properties.num.partitions", partitionNumber)
+                    }
+                }
+        )
+
+        AdminClient adminClient = createAdminClient()
+
+        when:
+        KafkaEmbedded kafkaEmbedded = applicationContext.getBean(KafkaEmbedded)
+
+        then:
+        kafkaEmbedded.kafkaServer.isPresent()
+        kafkaEmbedded.zkPort.isPresent()
+
+        and:
+        adminClient
+                .describeTopics([topicName]).values()
+                .get(topicName).get()
+                .partitions().size() == partitionNumber
+
+        cleanup:
+        adminClient.close()
+        applicationContext.close()
+    }
+
+    private static AdminClient createAdminClient() {
+        Properties properties = new Properties()
+        properties.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, AbstractKafkaConfiguration.DEFAULT_BOOTSTRAP_SERVERS)
+        AdminClient adminClient = AdminClient.create(properties)
+        adminClient
     }
 }
