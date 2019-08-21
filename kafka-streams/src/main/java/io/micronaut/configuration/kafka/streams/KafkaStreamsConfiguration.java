703ca1b506825b31c55e7f28af6e0d70b048da76
Wes Richardet <wes@tetriscodes.com>
2019-08-21 07:30:05 -0500

    Add javadoc comments to KafkaStreamsConfiguration

--- kafka-streams/src/main/java/io/micronaut/configuration/kafka/streams/KafkaStreamsConfiguration.java
+++ kafka-streams/src/main/java/io/micronaut/configuration/kafka/streams/KafkaStreamsConfiguration.java
@@ -2,6 +2,17 @@ package io.micronaut.configuration.kafka.streams;
 
 import java.util.Properties;
 
+/**
+ * Interface used in {@link ConfiguredStreamBuilder}.
+ *
+ * @since 2.0
+ * @author bobby
+ */
 public interface KafkaStreamsConfiguration {
+    /**
+     * The configuration.
+     *
+     * @return Properties for the streams configuration
+     */
     Properties getConfig();
 }
