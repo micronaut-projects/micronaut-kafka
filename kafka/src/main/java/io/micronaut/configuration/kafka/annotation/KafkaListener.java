0561addba7a0f7366b0aa6a0fd735f8f180bc064
graemerocher <graeme.rocher@gmail.com>
2020-06-18 13:00:33 +0200

    Ensure KafkaConsumerProcessor is initialized before polling

--- kafka/src/main/java/io/micronaut/configuration/kafka/annotation/KafkaListener.java
+++ kafka/src/main/java/io/micronaut/configuration/kafka/annotation/KafkaListener.java
@@ -35,6 +35,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
 @MessageListener
+@DefaultScope(Context.class)
 public @interface KafkaListener {
 
     /**
