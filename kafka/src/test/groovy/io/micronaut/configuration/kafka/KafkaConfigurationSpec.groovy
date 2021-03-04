a00ee9936d517fdb8eb7302c55e4da03cecb8a4a
Burt Beckwith <burt@burtbeckwith.com>
2021-03-04 15:10:52 -0500

    added test to verify env properties override app properties, both list and single

--- kafka/src/test/groovy/io/micronaut/configuration/kafka/KafkaConfigurationSpec.groovy
+++ kafka/src/test/groovy/io/micronaut/configuration/kafka/KafkaConfigurationSpec.groovy
@@ -4,11 +4,15 @@ import io.micronaut.configuration.kafka.config.AbstractKafkaConfiguration
 import io.micronaut.configuration.kafka.config.AbstractKafkaConsumerConfiguration
 import io.micronaut.configuration.kafka.config.KafkaConsumerConfiguration
 import io.micronaut.context.ApplicationContext
+import io.micronaut.context.env.EnvironmentPropertySource
+import io.micronaut.context.env.MapPropertySource
 import org.apache.kafka.clients.consumer.Consumer
 import org.apache.kafka.common.serialization.StringDeserializer
 import spock.lang.AutoCleanup
+import spock.lang.Issue
 import spock.lang.Specification
 
+import static io.micronaut.context.env.PropertySource.PropertyConvention.ENVIRONMENT_VARIABLE
 import static org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG
 import static org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG
 import static org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG
@@ -129,4 +133,127 @@ class KafkaConfigurationSpec extends Specification {
         cleanup:
         consumer.close()
     }
+
+    @Issue('https://github.com/micronaut-projects/micronaut-kafka/issues/286')
+    void 'test environment property overrides'() {
+        given:
+        def yamlPropertySource = new FakeYamlPropertySource()
+        def environmentPropertySource = new FakeEnvPropertySource()
+
+        Map yamlListConfig = ['kafka.bootstrap.servers[0]': 'localhost:1111',
+                              'kafka.bootstrap.servers': ['localhost:1111', 'localhost:2222'],
+                              'kafka.bootstrap-servers': ['localhost:1111', 'localhost:2222'],
+                              'kafka.bootstrap.servers[1]': 'localhost:2222']
+
+        Map yamlSingleConfig = ['kafka.bootstrap.servers': 'localhost:1111',
+                                'kafka.bootstrap-servers': 'localhost:1111']
+
+        Map envListConfig = [KAFKA_BOOTSTRAP_SERVERS: 'localhost:3333,localhost:4444']
+
+        Map envSingleConfig = [KAFKA_BOOTSTRAP_SERVERS: 'localhost:3333']
+
+        yamlPropertySource.map.putAll yamlListConfig
+        environmentPropertySource.map.putAll envListConfig
+
+        when: 'only yaml source, list, expect 1111,2222'
+        applicationContext = ApplicationContext.builder()
+                .propertySources(yamlPropertySource)
+                .start()
+        def config = applicationContext.getBean(AbstractKafkaConsumerConfiguration)
+        Properties props = config.config
+
+        then:
+        props[BOOTSTRAP_SERVERS_CONFIG] == 'localhost:1111,localhost:2222'
+
+        when: 'only env source, list, expect 3333,4444'
+        applicationContext.close()
+        applicationContext = ApplicationContext.builder()
+                .propertySources(environmentPropertySource)
+                .start()
+        config = applicationContext.getBean(AbstractKafkaConsumerConfiguration)
+        props = config.config
+
+        then:
+        props[BOOTSTRAP_SERVERS_CONFIG] == 'localhost:3333,localhost:4444'
+
+        when: 'both sources, both list, expect 3333,4444'
+        applicationContext.close()
+        applicationContext = ApplicationContext.builder()
+                .propertySources(yamlPropertySource, environmentPropertySource)
+                .start()
+        config = applicationContext.getBean(AbstractKafkaConsumerConfiguration)
+        props = config.config
+
+        then:
+        props[BOOTSTRAP_SERVERS_CONFIG] == 'localhost:3333,localhost:4444'
+
+        when: 'both sources, single yaml, list env, expect 3333,4444'
+
+        yamlPropertySource.map.putAll yamlSingleConfig
+        environmentPropertySource.map.putAll envListConfig
+
+        applicationContext.close()
+        applicationContext = ApplicationContext.builder()
+                .propertySources(yamlPropertySource, environmentPropertySource)
+                .start()
+        config = applicationContext.getBean(AbstractKafkaConsumerConfiguration)
+        props = config.config
+
+        then:
+        props[BOOTSTRAP_SERVERS_CONFIG] == 'localhost:3333,localhost:4444'
+
+        when: 'both sources, list yaml, single env, expect 3333'
+
+        yamlPropertySource.map.putAll yamlListConfig
+        environmentPropertySource.map.putAll envSingleConfig
+
+        applicationContext.close()
+        applicationContext = ApplicationContext.builder()
+                .propertySources(yamlPropertySource, environmentPropertySource)
+                .start()
+        config = applicationContext.getBean(AbstractKafkaConsumerConfiguration)
+        props = config.config
+
+        then:
+        props[BOOTSTRAP_SERVERS_CONFIG] == 'localhost:3333'
+
+        when: 'both sources, both single, expect 3333'
+
+        yamlPropertySource.map.putAll yamlSingleConfig
+        environmentPropertySource.map.putAll envSingleConfig
+
+        applicationContext.close()
+        applicationContext = ApplicationContext.builder()
+                .propertySources(yamlPropertySource, environmentPropertySource)
+                .start()
+        config = applicationContext.getBean(AbstractKafkaConsumerConfiguration)
+        props = config.config
+
+        then:
+        props[BOOTSTRAP_SERVERS_CONFIG] == 'localhost:3333'
+    }
+}
+
+class FakeYamlPropertySource extends MapPropertySource {
+
+    static final Map map = [:]
+
+    FakeYamlPropertySource() {
+        super('fake yml', map)
+    }
+
+    final int order = EnvironmentPropertySource.POSITION - 1
+}
+
+class FakeEnvPropertySource extends MapPropertySource {
+
+    static final Map map = [:]
+
+    FakeEnvPropertySource() {
+        super('fake env', map)
+    }
+
+    final int order = EnvironmentPropertySource.POSITION
+
+    final PropertyConvention convention = ENVIRONMENT_VARIABLE
 }
