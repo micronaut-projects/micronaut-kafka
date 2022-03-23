2c644b7332aac60ac86047657fb5577114b9fd93
Tim Yates <tim.yates@gmail.com>
2022-03-23 09:42:39 +0000

    Put tests back

--- kafka/src/test/groovy/io/micronaut/configuration/kafka/errors/KafkaErrorsSpec.groovy
+++ kafka/src/test/groovy/io/micronaut/configuration/kafka/errors/KafkaErrorsSpec.groovy
@@ -64,6 +64,34 @@ class KafkaErrorsSpec extends AbstractEmbeddedServerSpec {
         producer = context.getBean(TestProducer)
     }
 
+    void "should correctly handle error strategies"() {
+        when:
+        IntStream.range(0, 30).forEach { i -> producer.send(UUID.randomUUID(), new TestEvent(i)) }
+
+        then:
+        conditions.eventually {
+            listenerWithErrorStrategyResumeAtNextRecord.exceptions.size() == 1
+            listenerWithErrorStrategyResumeAtNextRecord.failed.size() == 1
+            listenerWithErrorStrategyResumeAtNextRecord.events.size() == 29
+
+            listenerWithErrorStrategyRetryOnError.exceptions.size() == 1
+            listenerWithErrorStrategyRetryOnError.failed.size() == 2 // One retry
+            listenerWithErrorStrategyRetryOnError.events.size() == 29
+
+            listenerWithErrorStrategyRetryOnError10Times.exceptions.size() == 1
+            listenerWithErrorStrategyRetryOnError10Times.failed.size() == 11 // 10 times retry
+            listenerWithErrorStrategyRetryOnError10Times.events.size() == 29
+
+            listenerSyncPerRecordWithErrorStrategyRetryOnError10Times.exceptions.size() == 1
+            listenerSyncPerRecordWithErrorStrategyRetryOnError10Times.failed.size() == 11 // 10 times retry
+            listenerSyncPerRecordWithErrorStrategyRetryOnError10Times.events.size() == 29
+
+            listenerWithErrorStrategyNone.exceptions.size() == 1
+            listenerWithErrorStrategyNone.failed.size() == 1
+            listenerWithErrorStrategyNone.events.stream().anyMatch(e -> e.count == 29)
+        }
+    }
+
     @Introspected
     @EqualsAndHashCode
     static class TestEvent implements Comparable<TestEvent> {
--- kafka/src/test/groovy/io/micronaut/configuration/kafka/errors/KafkaPartitionedErrorsSpec.groovy
+++ kafka/src/test/groovy/io/micronaut/configuration/kafka/errors/KafkaPartitionedErrorsSpec.groovy
@@ -4,8 +4,6 @@ import spock.lang.Ignore
 import spock.lang.Stepwise
 import spock.lang.Retry
 
-import java.util.stream.IntStream
-
 @Stepwise
 @Retry
 @Ignore("https://github.com/micronaut-projects/micronaut-kafka/issues/514")
@@ -19,34 +17,6 @@ class KafkaPartitionedErrorsSpec extends KafkaErrorsSpec {
     @Override
     protected Map<String, Object> getConfiguration() {
         return super.getConfiguration() + ['spec.name': KafkaErrorsSpec.class.simpleName]
-    }
-
-    void "should correctly handle error strategies"() {
-        when:
-        IntStream.range(0, 30).forEach { i -> producer.send(UUID.randomUUID(), new TestEvent(i)) }
-
-        then:
-        conditions.eventually {
-            listenerWithErrorStrategyResumeAtNextRecord.exceptions.size() == 1
-            listenerWithErrorStrategyResumeAtNextRecord.failed.size() == 1
-            listenerWithErrorStrategyResumeAtNextRecord.events.size() == 29
-
-            listenerWithErrorStrategyRetryOnError.exceptions.size() == 1
-            listenerWithErrorStrategyRetryOnError.failed.size() == 2 // One retry
-            listenerWithErrorStrategyRetryOnError.events.size() == 29
-
-            listenerWithErrorStrategyRetryOnError10Times.exceptions.size() == 1
-            listenerWithErrorStrategyRetryOnError10Times.failed.size() == 11 // 10 times retry
-            listenerWithErrorStrategyRetryOnError10Times.events.size() == 29
-
-            listenerSyncPerRecordWithErrorStrategyRetryOnError10Times.exceptions.size() == 1
-            listenerSyncPerRecordWithErrorStrategyRetryOnError10Times.failed.size() == 11 // 10 times retry
-            listenerSyncPerRecordWithErrorStrategyRetryOnError10Times.events.size() == 29
-
-            listenerWithErrorStrategyNone.exceptions.size() == 1
-            listenerWithErrorStrategyNone.failed.size() == 1
-            listenerWithErrorStrategyNone.events.stream().anyMatch(e -> e.count == 29)
-        }
     }
 
     void "should be correctly processing in partitions"() {
