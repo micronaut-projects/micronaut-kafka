1e5387c16610453bd91e67c41e566af6249f21a0
agustin-tecso <70208597+agustin-tecso@users.noreply.github.com>
2023-08-16 09:08:38 -0300

    Missing @Singleton (#633)
    * Missing @Singleton
    
    The class needs to be annotated with @Singleton so that it can be injected
    
    * Move example classes to a different java package
    
    Reason: io.micronaut.context.exceptions.NoSuchBeanException:
    No bean of type [io.micronaut.configuration.kafka.docs.producer.fallback.MessageClientFallback] exists.
    The bean [MessageClientFallback] is disabled because it is within the package [io.micronaut.configuration.kafka] which is disabled due to bean requirements:
      * Property [kafka.enabled] with value [false] should not equal: false
    
    * Add test to make sure that the fallback bean exists in the context
    
    ---------
    
    Co-authored-by: Guillermo Calvo <calvog@objectcomputing.com>

--- kafka/src/test/groovy/docs/producer/fallback/MessageClientFallback.java
+++ kafka/src/test/groovy/docs/producer/fallback/MessageClientFallback.java
@@ -1,14 +1,17 @@
-package io.micronaut.configuration.kafka.docs.producer.fallback;
+package docs.producer.fallback;
 
 // tag::imports[]
 import io.micronaut.context.annotation.Replaces;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.util.StringUtils;
+import jakarta.inject.Singleton;
 // end::imports[]
 
+@Requires(property = "spec.name", value = "MessageClientFallbackSpec")
 // tag::clazz[]
 @Requires(property = "kafka.enabled", notEquals = StringUtils.TRUE, defaultValue = StringUtils.TRUE) // <1>
 @Replaces(MessageClient.class) // <2>
+@Singleton
 public class MessageClientFallback implements MessageClient { // <3>
 
     @Override
---
+++ kafka/src/test/groovy/docs/producer/fallback/MessageClientFallbackSpec.groovy
@@ -0,0 +1,30 @@
+package docs.producer.fallback
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.annotation.Property
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import jakarta.inject.Inject
+import spock.lang.Specification
+
+@MicronautTest
+@Property(name = "spec.name", value = "MessageClientFallbackSpec")
+@Property(name = "kafka.enabled", value = "false")
+class MessageClientFallbackSpec extends Specification {
+
+    @Inject
+    ApplicationContext context
+
+    void "test that context contains the fallback bean"() {
+        when:
+        MessageClientFallback bean = context.getBean(MessageClientFallback)
+
+        then:
+        bean != null
+
+        when:
+        bean.send('message')
+
+        then:
+        thrown(UnsupportedOperationException)
+    }
+}
