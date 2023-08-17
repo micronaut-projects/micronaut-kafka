c410c1f23a6ba293c6cf7d8a5c16e1f8d2d4fba1
Dean Wette <wetted@unityfoundation.io>
2023-08-17 16:14:12 -0500

    remove unused imports

--- test-suite-kotlin/src/test/kotlin/io/micronaut/kafka/docs/quickstart/QuickstartTest.kt
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/kafka/docs/quickstart/QuickstartTest.kt
@@ -2,12 +2,9 @@ package io.micronaut.kafka.docs.quickstart
 
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.annotation.Property
-import io.micronaut.kafka.docs.AbstractKafkaTest
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
 
 @Property(name = "spec.name", value = "QuickstartTest")
 @MicronautTest
