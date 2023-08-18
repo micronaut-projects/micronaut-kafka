0e575af6267b55b1537811ffaec571edd19bc010
Tim Yates <tim.yates@gmail.com>
2023-08-18 14:09:43 +0100

    Groovify the groovy test-suite (#824)
    Remove awaitility
    remove static container
    stop stopping the container after tests

--- test-suite-groovy/build.gradle.kts
+++ test-suite-groovy/build.gradle.kts
@@ -9,7 +9,6 @@ dependencies {
     testImplementation(libs.testcontainers.kafka)
     testImplementation(mnTest.micronaut.test.spock)
     testRuntimeOnly(libs.junit.jupiter.engine)
-    testImplementation(libs.awaitility)
     testImplementation(mnReactor.micronaut.reactor)
     testImplementation(mnSerde.micronaut.serde.jackson)
     testImplementation(projects.micronautKafka)
