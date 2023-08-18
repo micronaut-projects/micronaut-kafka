60a0f3bc63a5cb993101dc9e67ad369eccd6a49c
Dean Wette <wetted@unityfoundation.io>
2023-08-18 01:19:22 -0500

    Multi-language documentation for Committing Offsets Manually with Batch example (#820)
    * Multi-language documentation for Committing Offsets Manually with Batch example
    
    closes #815
    
    * changes for review feedback
    
    * idiomatic kotlin

--- test-suite/build.gradle.kts
+++ test-suite/build.gradle.kts
@@ -9,6 +9,8 @@ dependencies {
     testImplementation(mnTest.micronaut.test.junit5)
     testRuntimeOnly(libs.junit.jupiter.engine)
     testImplementation(libs.awaitility)
+    testImplementation(mnReactor.micronaut.reactor)
+    testImplementation(mnSerde.micronaut.serde.jackson)
     testImplementation(projects.micronautKafka)
 }
 
