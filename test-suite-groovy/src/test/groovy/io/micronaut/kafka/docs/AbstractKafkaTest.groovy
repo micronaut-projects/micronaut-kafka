614dd7a31ddbfe830d3b8940f2e4c46c103101c7
Tim Yates <tim.yates@gmail.com>
2023-08-18 09:45:08 +0100

    Groovify the groovy test-suite
    Remove awaitility
    remove static container
    stop stopping the container after tests

--- test-suite-groovy/src/test/groovy/io/micronaut/kafka/docs/AbstractKafkaTest.groovy
+++ test-suite-groovy/src/test/groovy/io/micronaut/kafka/docs/AbstractKafkaTest.groovy
@@ -3,25 +3,20 @@ package io.micronaut.kafka.docs
 import io.micronaut.test.support.TestPropertyProvider
 import org.testcontainers.containers.KafkaContainer
 import org.testcontainers.utility.DockerImageName
+import spock.lang.AutoCleanup
+import spock.lang.Shared
 import spock.lang.Specification
 
-/**
- * @see <a href="https://www.testcontainers.org/test_framework_integration/manual_lifecycle_control/#singleton-containers">Singleton containers</a>
- */
 abstract class AbstractKafkaTest extends Specification implements TestPropertyProvider {
 
-    static final KafkaContainer MY_KAFKA
-
-    static {
-        MY_KAFKA = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"))
-        MY_KAFKA.start()
-    }
+    @Shared
+    @AutoCleanup
+    KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"))
 
     @Override
     Map<String, String> getProperties() {
-        while (!MY_KAFKA.isRunning()) {
-            MY_KAFKA.start()
-        }
-        ["kafka.bootstrap.servers": MY_KAFKA.getBootstrapServers()]
+        kafkaContainer.start()
+
+        ["kafka.bootstrap.servers": kafkaContainer.getBootstrapServers()]
     }
 }
