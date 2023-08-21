905a3130d0279945ad1a4205c108d8c8f450a348
Dean Wette <wetted@unityfoundation.io>
2023-08-21 09:40:38 -0500

    use @Canonical

--- test-suite-groovy/src/test/groovy/io/micronaut/kafka/docs/producer/inject/Book.groovy
+++ test-suite-groovy/src/test/groovy/io/micronaut/kafka/docs/producer/inject/Book.groovy
@@ -1,12 +1,10 @@
 package io.micronaut.kafka.docs.producer.inject
 
+import groovy.transform.Canonical
 import io.micronaut.serde.annotation.Serdeable
 
 @Serdeable
+@Canonical
 class Book {
     String title
-
-    Book(String title) {
-        this.title = title
-    }
 }
