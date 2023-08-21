74ce5541a19e63b51be3e39007ecc6fab9c85627
Dean Wette <wetted@unityfoundation.io>
2023-08-21 10:17:29 -0500

    add @Requires to avoid bean pollution

--- test-suite/src/test/java/io/micronaut/kafka/docs/producer/config/ProductClient.java
+++ test-suite/src/test/java/io/micronaut/kafka/docs/producer/config/ProductClient.java
@@ -3,9 +3,11 @@ package io.micronaut.kafka.docs.producer.config;
 // tag::imports[]
 import io.micronaut.configuration.kafka.annotation.KafkaClient;
 import io.micronaut.context.annotation.Property;
+import io.micronaut.context.annotation.Requires;
 import org.apache.kafka.clients.producer.ProducerConfig;
 // end::imports[]
 
+@Requires(property = "spec.name", value = "ProductClientTest")
 // tag::clazz[]
 @KafkaClient(
     id = "product-client",
