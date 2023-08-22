c6d010c953892487e77c8f973d1fab8f313a3559
Dean Wette <wetted@unityfoundation.io>
2023-08-22 15:46:51 -0500

    Multi-language docs examples for Specifying error strategies

---
+++ test-suite-groovy/src/test/groovy/io/micronaut/kafka/docs/consumer/errors/ExceptionProductListener.groovy
@@ -0,0 +1,24 @@
+package io.micronaut.kafka.docs.consumer.errors
+
+import io.micronaut.configuration.kafka.annotation.ErrorStrategy
+import io.micronaut.configuration.kafka.annotation.ErrorStrategyValue
+import io.micronaut.configuration.kafka.annotation.KafkaListener
+import io.micronaut.context.annotation.Requires
+
+@Requires(property = 'spec.name', value = 'ExceptionProductListenerTest')
+// tag::annotation[]
+@KafkaListener(
+    value = 'myGroup',
+    errorStrategy = @ErrorStrategy(
+            value = ErrorStrategyValue.RETRY_ON_ERROR,
+            retryDelay = '50ms',
+            retryCount = 3,
+            exceptionTypes = [ MyException, MySecondException ]
+    )
+)
+// end::annotation[]
+class ExceptionProductListener {
+}
+
+class MyException extends Exception {}
+class MySecondException extends Exception {}
