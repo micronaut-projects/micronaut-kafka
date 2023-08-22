95b0d51d9af381c38aee5fd85d30a268df6ae7db
Dean Wette <wetted@unityfoundation.io>
2023-08-22 12:08:29 -0500

    add @Requires to avoid bean pollution

--- test-suite/src/test/java/io/micronaut/kafka/docs/consumer/config/ConsumerUniqueGroupIdListener.java
+++ test-suite/src/test/java/io/micronaut/kafka/docs/consumer/config/ConsumerUniqueGroupIdListener.java
@@ -1,7 +1,9 @@
 package io.micronaut.kafka.docs.consumer.config;
 
 import io.micronaut.configuration.kafka.annotation.KafkaListener;
+import io.micronaut.context.annotation.Requires;
 
+@Requires(property = "spec.name", value = "ConfigProductListenerTest")
 // tag::annotation[]
 @KafkaListener(groupId = "myGroup", uniqueGroupId = true)
 // end::annotation[]
