f26e5bbfa5d453e3647e84e8baa0801e972b4064
Dean Wette <wetted@unityfoundation.io>
2023-08-23 11:13:58 -0500

    Multi-language docs examples for Manually Committing Offsets examples

---
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/kafka/docs/consumer/offsets/manual/ProductListener.kt
@@ -0,0 +1,34 @@
+package io.micronaut.kafka.docs.consumer.offsets.manual
+
+// tag::imports[]
+import io.micronaut.configuration.kafka.annotation.KafkaListener
+import io.micronaut.configuration.kafka.annotation.Topic
+import io.micronaut.context.annotation.Requires
+import io.micronaut.kafka.docs.Product
+import org.apache.kafka.clients.consumer.Consumer
+import org.apache.kafka.clients.consumer.OffsetAndMetadata
+import org.apache.kafka.common.TopicPartition
+import java.util.*
+
+import io.micronaut.configuration.kafka.annotation.OffsetReset.EARLIEST
+import io.micronaut.configuration.kafka.annotation.OffsetStrategy.DISABLED
+// end::imports[]
+
+@Requires(property = "spec.name", value = "ManualProductListenerTest")
+// tag::clazz[]
+@KafkaListener(offsetReset = EARLIEST, offsetStrategy = DISABLED) // <1>
+class ProductListener {
+
+    @Topic("awesome-products")
+    fun receive(product: Product, offset: Long, partition: Int, topic: String, kafkaConsumer: Consumer<*, *>) { // <2>
+        // process product record
+
+        // commit offsets
+        kafkaConsumer.commitSync(Collections.singletonMap( // <3>
+                TopicPartition(topic, partition),
+                OffsetAndMetadata(offset + 1, "my metadata")
+            )
+        )
+    }
+}
+// end::clazz[]
