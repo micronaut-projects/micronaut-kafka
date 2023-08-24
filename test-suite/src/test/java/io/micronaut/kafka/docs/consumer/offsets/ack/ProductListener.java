828f9909e9970e35a00564a7cb59c726a20cb29b
Dean Wette <wetted@unityfoundation.io>
2023-08-24 08:26:22 -0500

    Multi-language docs examples for Manually Committing Offsets examples (#835)

--- test-suite/src/test/java/io/micronaut/kafka/docs/consumer/offsets/ack/ProductListener.java
+++ test-suite/src/test/java/io/micronaut/kafka/docs/consumer/offsets/ack/ProductListener.java
@@ -1,24 +1,23 @@
-package io.micronaut.configuration.kafka.docs.consumer.offsets.ack;
+package io.micronaut.kafka.docs.consumer.offsets.ack;
 
 import io.micronaut.configuration.kafka.annotation.KafkaListener;
 import io.micronaut.configuration.kafka.annotation.Topic;
-import io.micronaut.configuration.kafka.docs.consumer.config.Product;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.kafka.docs.Product;
 import io.micronaut.messaging.Acknowledgement;
 
 import static io.micronaut.configuration.kafka.annotation.OffsetReset.EARLIEST;
 import static io.micronaut.configuration.kafka.annotation.OffsetStrategy.DISABLED;
 
-class ProductListener {
+@Requires(property = "spec.name", value = "AckProductListenerTest")
+// tag::clazz[]
+@KafkaListener(offsetReset = EARLIEST, offsetStrategy = DISABLED) // <1>
+public class ProductListener {
 
-    // tag::method[]
-    @KafkaListener(offsetReset = EARLIEST, offsetStrategy = DISABLED) // <1>
     @Topic("awesome-products")
-    void receive(Product product,
-                 Acknowledgement acknowledgement) { // <2>
-
+    public void receive(Product product, Acknowledgement acknowledgement) { // <2>
         // process product record
-
         acknowledgement.ack(); // <3>
     }
-    // end::method[]
 }
+// end::clazz[]
--- test-suite/src/test/java/io/micronaut/kafka/docs/consumer/offsets/manual/ProductListener.java
+++ test-suite/src/test/java/io/micronaut/kafka/docs/consumer/offsets/manual/ProductListener.java
@@ -1,9 +1,10 @@
-package io.micronaut.configuration.kafka.docs.consumer.offsets.manual;
+package io.micronaut.kafka.docs.consumer.offsets.manual;
 
 // tag::imports[]
 import io.micronaut.configuration.kafka.annotation.KafkaListener;
 import io.micronaut.configuration.kafka.annotation.Topic;
-import io.micronaut.configuration.kafka.docs.consumer.config.Product;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.kafka.docs.Product;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.TopicPartition;
@@ -14,16 +15,13 @@ import static io.micronaut.configuration.kafka.annotation.OffsetReset.EARLIEST;
 import static io.micronaut.configuration.kafka.annotation.OffsetStrategy.DISABLED;
 // end::imports[]
 
-class ProductListener {
+@Requires(property = "spec.name", value = "ManualProductListenerTest")
+// tag::clazz[]
+@KafkaListener(offsetReset = EARLIEST, offsetStrategy = DISABLED) // <1>
+public class ProductListener {
 
-    // tag::method[]
-    @KafkaListener(offsetReset = EARLIEST, offsetStrategy = DISABLED) // <1>
     @Topic("awesome-products")
-    void receive(Product product,
-                 long offset,
-                 int partition,
-                 String topic,
-                 Consumer kafkaConsumer) { // <2>
+    public void receive(Product product, long offset, int partition, String topic, Consumer kafkaConsumer) { // <2>
         // process product record
 
         // commit offsets
@@ -32,5 +30,5 @@ class ProductListener {
                 new OffsetAndMetadata(offset + 1, "my metadata")
         ));
     }
-    // end::method[]
 }
+// end::clazz[]
