4e6d9ac159cfe94a4c872f445e2e32baec407ca1
Dean Wette <wetted@unityfoundation.io>
2023-08-25 12:13:55 -0500

    code smell: add assertion

--- test-suite-kotlin/src/test/kotlin/io/micronaut/kafka/docs/consumer/config/ConfigProductListenerTest.kt
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/kafka/docs/consumer/config/ConfigProductListenerTest.kt
@@ -2,6 +2,7 @@ package io.micronaut.kafka.docs.consumer.config
 
 import io.micronaut.context.ApplicationContext
 import io.micronaut.kafka.docs.Product
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.util.Map
 
@@ -12,9 +13,11 @@ class ConfigProductListenerTest {
         ApplicationContext.run(
             Map.of<String, Any>("kafka.enabled", "true", "spec.name", "ConfigProductListenerTest")
         ).use { ctx ->
-            val product = Product("Blue Trainers", 5)
-            val client = ctx.getBean(ProductClient::class.java)
-            client.send("Nike", product)
+            Assertions.assertDoesNotThrow {
+                val product = Product("Blue Trainers", 5)
+                val client = ctx.getBean(ProductClient::class.java)
+                client.send("Nike", product)
+            }
         }
     }
 }
--- test-suite/src/test/java/io/micronaut/kafka/docs/consumer/config/ConfigProductListenerTest.java
+++ test-suite/src/test/java/io/micronaut/kafka/docs/consumer/config/ConfigProductListenerTest.java
@@ -6,16 +6,20 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
-public class ConfigProductListenerTest {
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class ConfigProductListenerTest {
 
     @Test
     void testSendProduct() {
         try (ApplicationContext ctx = ApplicationContext.run(
             Map.of("kafka.enabled", "true", "spec.name", "ConfigProductListenerTest")
         )) {
-            Product product = new Product("Blue Trainers", 5);
-            ProductClient client = ctx.getBean(ProductClient.class);
-            client.send("Nike", product);
+            assertDoesNotThrow(() -> {
+                Product product = new Product("Blue Trainers", 5);
+                ProductClient client = ctx.getBean(ProductClient.class);
+                client.send("Nike", product);
+            });
         }
     }
 }
