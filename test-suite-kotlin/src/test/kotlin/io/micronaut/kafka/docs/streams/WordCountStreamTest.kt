507a4cf3e75f48798cef51f7e1a25210eab1bc08
Dean Wette <wetted@unityfoundation.io>
2023-08-28 15:12:39 -0500

    cleanup kotlin test

--- test-suite-kotlin/src/test/kotlin/io/micronaut/kafka/docs/streams/WordCountStreamTest.kt
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/kafka/docs/streams/WordCountStreamTest.kt
@@ -2,9 +2,8 @@ package io.micronaut.kafka.docs.streams
 
 import io.micronaut.context.ApplicationContext
 import io.micronaut.core.util.StringUtils
-import org.awaitility.Awaitility
+import org.awaitility.Awaitility.await
 import org.junit.jupiter.api.Test
-import java.util.Map
 import java.util.concurrent.TimeUnit
 
 internal class WordCountStreamTest {
@@ -12,14 +11,14 @@ internal class WordCountStreamTest {
     @Test
     fun testWordCounter() {
         ApplicationContext.run(
-            Map.of<String, Any>("kafka.enabled", StringUtils.TRUE, "spec.name", "WordCountStreamTest")
+            mapOf("kafka.enabled" to StringUtils.TRUE, "spec.name" to "WordCountStreamTest")
         ).use { ctx ->
             val client = ctx.getBean(WordCountClient::class.java)
             client.publishSentence("test to test for words")
 
             val listener = ctx.getBean(WordCountListener::class.java)
 
-            Awaitility.await().atMost(10, TimeUnit.SECONDS).until {
+            await().atMost(10, TimeUnit.SECONDS).until {
                 listener.getWordCounts().size == 4 &&
                         listener.getCount("test") == 2L &&
                         listener.getCount("to") == 1L &&
