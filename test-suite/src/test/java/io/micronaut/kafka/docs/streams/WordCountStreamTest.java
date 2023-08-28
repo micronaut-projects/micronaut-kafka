4caeb151ca0a564647807425235b15dc783a038e
Dean Wette <wetted@unityfoundation.io>
2023-08-28 10:42:25 -0500

    change test [skip ci]

--- test-suite/src/test/java/io/micronaut/kafka/docs/streams/WordCountStreamTest.java
+++ test-suite/src/test/java/io/micronaut/kafka/docs/streams/WordCountStreamTest.java
@@ -1,23 +1,31 @@
 package io.micronaut.kafka.docs.streams;
 
 import io.micronaut.context.ApplicationContext;
+import io.micronaut.core.util.StringUtils;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
 
 class WordCountStreamTest {
 
     @Test
     void testWordCounter() {
         try (ApplicationContext ctx = ApplicationContext.run(
-            Map.of("kafka.enabled", "true", "spec.name", "WordCountStreamTest")
+            Map.of("kafka.enabled", StringUtils.TRUE, "spec.name", "WordCountStreamTest")
         )) {
-            assertDoesNotThrow(() -> {
-                WordCountClient client = ctx.getBean(WordCountClient.class);
-                client.publishSentence("Test to test for words");
-            });
+            WordCountClient client = ctx.getBean(WordCountClient.class);
+            client.publishSentence("test to test for words");
+            WordCountListener listener = ctx.getBean(WordCountListener.class);
+            await().atMost(10, SECONDS).until(() ->
+                listener.getWordCounts().size()       == 4 &&
+                    listener.getCount("test")  == 2 &&
+                    listener.getCount("to")    == 1 &&
+                    listener.getCount("for")   == 1 &&
+                    listener.getCount("words") == 1
+            );
         }
     }
 }
