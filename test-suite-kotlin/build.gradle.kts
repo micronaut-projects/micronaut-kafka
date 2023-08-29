48051c9fed146039ca80bac149a5e41358d758f9
Dean Wette <wetted@unityfoundation.io>
2023-08-29 11:09:17 -0500

    Cleanup builds for test-suite modules

--- test-suite-kotlin/build.gradle.kts
+++ test-suite-kotlin/build.gradle.kts
@@ -1,22 +1,11 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.9.10"
-    id("org.jetbrains.kotlin.kapt") version "1.9.10"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.kapt)
     id("io.micronaut.internal.build.kafka-testsuite")
 }
 
 dependencies {
     kaptTest(platform(mn.micronaut.core.bom))
     kaptTest(mn.micronaut.inject.java)
-    testImplementation(mn.micronaut.messaging)
-    testImplementation(mnSerde.micronaut.serde.jackson)
-    testImplementation(libs.testcontainers.kafka)
     testImplementation(mnTest.micronaut.test.junit5)
-    testRuntimeOnly(libs.junit.jupiter.engine)
-    testRuntimeOnly(mnLogging.logback.classic)
-    testImplementation(libs.awaitility)
-    testImplementation(mnReactor.micronaut.reactor)
-    testImplementation(mnSerde.micronaut.serde.jackson)
-    testImplementation(mnRxjava2.micronaut.rxjava2)
-    testImplementation(projects.micronautKafka)
-    testImplementation(projects.micronautKafkaStreams)
 }
