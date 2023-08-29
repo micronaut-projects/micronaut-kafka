8e5b69105695e51587abf33e1f08fc54b7deedc6
Harrison Cole <harrisoncole05@gmail.com>
2023-08-29 15:19:03 +1000

    Remove unused 'io.micronaut.context.annotation.Bean' import

--- kafka/src/main/java/io/micronaut/configuration/kafka/executor/ConsumerExecutorServiceConfig.java
+++ kafka/src/main/java/io/micronaut/configuration/kafka/executor/ConsumerExecutorServiceConfig.java
@@ -15,7 +15,6 @@
  */
 package io.micronaut.configuration.kafka.executor;
 
-import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.scheduling.TaskExecutors;
