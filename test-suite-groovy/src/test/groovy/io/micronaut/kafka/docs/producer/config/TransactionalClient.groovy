edb6e8b94723f5ae515492b019ffea8df57e300f
Guillermo Calvo <guillermo@guillermo.dev>
2023-09-29 11:25:15 +0200

    Add code snippets

---
+++ test-suite-groovy/src/test/groovy/io/micronaut/kafka/docs/producer/config/TransactionalClient.groovy
@@ -0,0 +1,12 @@
+package io.micronaut.kafka.docs.producer.config
+
+import io.micronaut.configuration.kafka.annotation.KafkaClient
+import io.micronaut.context.annotation.Requires
+
+@Requires(property = "spec.name", value = "TransactionalClientTest")
+// tag::clazz[]
+@KafkaClient(id = "my-client", transactionalId = "my-tx-id")
+interface TransactionalClient {
+    // define client API
+}
+// end::clazz[]
