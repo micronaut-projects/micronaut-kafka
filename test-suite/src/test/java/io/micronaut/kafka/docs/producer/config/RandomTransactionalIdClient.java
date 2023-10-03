b5b29443121e59cfdc9365b2b59ce8a240a1a639
Guillermo Calvo <guillermo@guillermo.dev>
2023-10-03 12:41:06 +0200

    Transactional Kafka Producer per thread (#882)
    * Update documentation
    
    * Add code snippets

---
+++ test-suite/src/test/java/io/micronaut/kafka/docs/producer/config/RandomTransactionalIdClient.java
@@ -0,0 +1,14 @@
+package io.micronaut.kafka.docs.producer.config;
+
+import io.micronaut.configuration.kafka.annotation.KafkaClient;
+import io.micronaut.context.annotation.Prototype;
+import io.micronaut.context.annotation.Requires;
+
+@Requires(property = "spec.name", value = "RandomTransactionalIdClientTest")
+// tag::clazz[]
+@Prototype
+@KafkaClient(id = "my-client", transactionalId = "my-tx-id-${random.uuid}")
+public interface RandomTransactionalIdClient {
+    // define client API
+}
+// end::clazz[]
