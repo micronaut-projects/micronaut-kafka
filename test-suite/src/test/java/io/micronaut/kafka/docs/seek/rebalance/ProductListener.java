21b9e9e1c8ed5940ecc295d40666a14c82de62ef
Ayoub Bouria <69218182+ayoubouria@users.noreply.github.com>
2025-12-17 16:53:11 +0100

    fix imports

--- test-suite/src/test/java/io/micronaut/kafka/docs/seek/rebalance/ProductListener.java
+++ test-suite/src/test/java/io/micronaut/kafka/docs/seek/rebalance/ProductListener.java
@@ -1,11 +1,14 @@
 package io.micronaut.kafka.docs.seek.rebalance;
 
 import io.micronaut.configuration.kafka.ConsumerAware;
-import io.micronaut.configuration.kafka.annotation.*;
+import io.micronaut.configuration.kafka.annotation.KafkaListener;
+import io.micronaut.configuration.kafka.annotation.OffsetReset;
+import io.micronaut.configuration.kafka.annotation.Topic;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.kafka.docs.Product;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.jspecify.annotations.NonNull;
-import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.TopicPartition;
 import java.util.*;
 
