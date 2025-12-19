9d2c3fb500d2453de14484a6986a080398328531
issam liqali <liqaliissam@gmail.com>
2025-12-19 11:34:38 +0100

    add spock version

--- test-suite-groovy/build.gradle.kts
+++ test-suite-groovy/build.gradle.kts
@@ -8,3 +8,10 @@ dependencies {
     testCompileOnly(mn.micronaut.inject.groovy)
     testImplementation(mnTest.micronaut.test.spock)
 }
+
+//TODO remove once Micronaut Test ships Spock version compatible with Groovy 5
+configurations.all {
+    resolutionStrategy {
+        force("org.spockframework:spock-core:2.4-M7-groovy-5.0")
+    }
+}
