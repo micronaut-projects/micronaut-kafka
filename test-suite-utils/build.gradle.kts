bc046ef7b5db9014f33c4bd65508dc2dcbf5fdfe
issam liqali <liqaliissam@gmail.com>
2025-12-19 11:36:43 +0100

    remove test-resources

---
+++ test-suite-utils/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("io.micronaut.build.internal.java-base")
+}
+dependencies {
+    implementation(platform(mnTest.boms.testcontainers))
+    implementation(libs.testcontainers.kafka)
+}
