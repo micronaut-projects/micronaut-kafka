e5c2bdb4a68199f4fba611584f2bc59514bff78e
issam liqali <liqaliissam@gmail.com>
2025-12-21 15:25:00 +0100

    configure kafka testcontainer

--- kafka-streams/src/test/groovy/io/micronaut/configuration/kafka/streams/AbstractKafkaContainerSpec.groovy
+++ kafka-streams/src/test/groovy/io/micronaut/configuration/kafka/streams/AbstractKafkaContainerSpec.groovy
@@ -4,12 +4,21 @@ import io.micronaut.context.ApplicationContext
 import spock.lang.AutoCleanup
 import spock.lang.Shared
 
+import org.testcontainers.kafka.KafkaContainer
+import org.testcontainers.utility.DockerImageName
+
 abstract class AbstractKafkaContainerSpec extends AbstractKafkaSpec {
 
     @Shared @AutoCleanup ApplicationContext context
     @Shared String bootstrapServers
+    @Shared @AutoCleanup KafkaContainer kafkaContainer
 
     void setupSpec() {
+        def kafkaImage = DockerImageName
+                .parse("apache/kafka:latest")
+        kafkaContainer = new KafkaContainer(kafkaImage)
+        kafkaContainer.start()
+
         startContext()
         afterKafkaStarted()
     }
@@ -26,6 +35,12 @@ abstract class AbstractKafkaContainerSpec extends AbstractKafkaSpec {
 
     void stopContext() {
         context?.stop()
+    }
+
+    protected Map<String, Object> getConfiguration() {
+        def config = super.getConfiguration()
+        config['kafka.bootstrap.servers'] = kafkaContainer.getBootstrapServers()
+        config
     }
 
     void cleanupSpec() {
